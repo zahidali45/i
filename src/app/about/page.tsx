3d2a0ef75d2cf712ccb701373b91c3fa0252f477
tsc <mz260959@gmail.com>
2024-09-01 20:49:48 +0500

    project

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>my name is muhammad zahid from ruknpur adda suigas moza adam arain sardargarh rahim yar khan</div>
+  )
+}
+
+export default page
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>home page</div>
+    <div>my contact number is 03013987385</div>
   )
 }
 
